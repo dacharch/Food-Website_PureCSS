05fe1427b3363ff0e9b515f2528ed08a59144db6
Neeraj Kumar <nk35715945@gmail.com>
2024-04-13 11:45:27 +0530

    Hero Section is Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
+import Home from "./components/pages/Home/Home";
+import { Cart } from "./components/pages/Cart/Cart";
+import PlaceOrder from "./components/pages/PlaceOrder/PlaceOrder";
 
 function App() {
   return (
      <div className="app">
        <NavBar/>
+       <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/order" element={<PlaceOrder/>} />
+       </Routes>
      </div>
     
   );
---
+++ src/components/ExploreMenu/ExploreMenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ExploreMenu.css"
+
+const ExploreMenu = () => {
+  return (
+    <div>
+        ExploreMenu
+    </div>
+  )
+}
+
+export default ExploreMenu
